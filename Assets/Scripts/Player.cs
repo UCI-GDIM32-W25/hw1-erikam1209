ca57f3351c1b1bb9574383f2ea8b67443e4e9c79
erikam1209 <emcphetr@uci.edu>
2025-01-09 14:09:08 -0800

    Player Moves
    Player object is hooked up to the Player script
    Player is able to move with WASD

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,23 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
     }
 
     public void PlantSeed ()
